304737c8b6376e7ed5903907976049154b8bd569
HisenMirrou <149622836+HisenMirrou@users.noreply.github.com>
2024-03-26 16:56:59 +0300

    Pyton tasks 1-2

---
+++ Lesson3task2Rozno.py
@@ -0,0 +1,23 @@
+print('Впишите по порядку стадии развития человека:')
+print(1) #Ardipithecus
+a1 = input()
+print(2)  #Australopithecus
+a2 = input()
+print(3) #Paranthropus
+a3 = input()
+print(4) #Homo habilis
+a4 = input()
+print(5) #Homo erectus
+a5 = input()
+print(6) #Homo heidelbergensis
+a6 = input()
+print(7) #Homo floresiensis
+a7 = input()
+print(8) # Homo neanderthalensis
+a8 = input()
+print(9) #Homo sapiens
+a9 = input()
+print(10) #Homo sapiens sapiens
+a10 = input()
+print( a1, a2 , a3, a4, a5, a6,a7, a8, a9, a10, sep='=>' )
+
