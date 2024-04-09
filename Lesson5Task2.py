b2fb08591d3b7220c7403e21cff26bebe0df40de
HisenMirrou <149622836+HisenMirrou@users.noreply.github.com>
2024-04-09 18:22:37 +0300

    Phyton lessons 8-9 tasks

--- Lesson5Task2.py
+++ Lesson5Task2.py
@@ -5,11 +5,51 @@
 word = input()
 countV = 0
 countC = 0
-A = ['a','e','o','u','i']
+A = 'a'; A1 = 0
+E = 'e'; E1 = 0
+I = 'i'; I1 = 0
+O = 'o'; O1 = 0
+U = 'u';U1 = 0
+
 for i in word:
     if i in A:
-        countV +=1
+        A1 += 1
+        countV += 1
+    elif i in E:
+        E1 += 1
+        countV += 1
+    elif i in I:
+        I1 += 1
+        countV += 1
+    elif i in O:
+        O1 += 1
+        countV += 1
+    elif i in U:
+        U1 += 1
+        countV += 1
     else:
         countC += 1
-print(countV)
-print(countC)
+
+if A1 > 0:
+    print("a =", A1)
+else:
+    print("a = false")
+if E1 > 0:
+    print("e =", E1)
+else:
+    print("e = false")
+if I1 > 0:
+    print("i =", I1)
+else:
+    print("i = false")
+if O1 > 0:
+    print("o =", O1)
+else:
+    print("o = false")
+if U1 > 0:
+    print("u =", U1)
+else:
+    print("u = false")
+
+print("Количество гласных", countV)
+print("Количество согласных", countC)
